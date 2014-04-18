06bad6bd2642641ffb22450457f4b31bea586a7e
Eli Gonzalez <004086484@coyote.csusb.edu>
2014-04-17 21:49:09 -0700

    Header for VM

--- Assembler.cpp
+++ Assembler.cpp
@@ -6,69 +6,9 @@
 #include "Assembler.h"
 #include <Algorithm>
 #include <sstream>
-#include <math.h>
+#include "conversions.h"
 
 using namespace std;
-
-string dtb(int x, int sz){
-	string s = "";
-	while(x > 0){
-		int bin = x % 2;
-		stringstream out;
-		out << bin;
-		s += out.str();
-		x = x/2;
-	}
-	reverse(s.begin(), s.end());
-	if(s.size() < sz) {
-		for(int i = 0; i <= s.size() - sz; i++) {
-			s.insert(0, "0");
-		}
-	}
-	return s;
-}
-
-string dtb2(int x, int sz){
-	if(x >= 0){
-		return dtb(x, sz);          //If positive just call decimal to binary
-	}
-	else {
-		string y;
-		x *= -1;                    //Negate the number
-		y = dtb(x, sz);             //Take the number get the binary form
-                                    //Take the 2's complement now
-                                    //Negate the entire thing
-		for (int i = 0; i < y.size(); i++) {
-			if(y[i] == '1') { 
-				y[i] = '0';
-			}
-			else {
-				y[i] = '1';
-			}
-		}
-		//Add 1 to the code
-		for(int i = y.size() - 1; i >= 0; i--) {
-			if(y[i] == '0') {
-				y[i] = '1';
-				break;
-			}
-			else {
-				y[i] = '0';
-			}
-		}
-		return y;
-	}
-}
-
-int btd(string code){
-	int decimal = 0;
-	for(int i = 0; i < code.size(); i++) {
-		if(code[i] == '1'){
-			decimal += pow(2, code.size() - i - 1);
-		}
-	}
-	return decimal;
-}
 
 // Instruction format functions
 
---
+++ VirtualMachine.h
@@ -0,0 +1,56 @@
+#include <vector>
+#include <string>
+#include <map>
+
+class VirtualMachine {
+
+private:
+  static const int REG_FILE_SIZE = 4;
+  static const int MEM_SIZE = 256;
+  vector <int> r;
+  vector <int> mem;
+  int pc, ir, sr, sp, clock, base, limit;
+  typedef void (VirtualMachine::*function)();
+  map<string, function> functions;
+  ifstream o, in;
+  ofstream out;
+public:
+  VirtualMachine(string filename);
+  //These are all available operation by the VM
+  void load();
+  void loadi();
+  void store();
+  void add();
+  void addi();
+  void addc();
+  void addci();
+  void sub();
+  void subi();
+  void subc();
+  void subci();
+  void ander();
+  void andi();
+  void xorer();
+  void xori();
+  void negate();
+  void shl();
+  void shla();
+  void shr();
+  void shra();
+  void compr();
+  void compri();
+  void getstat();
+  void putstat();
+  void jump();
+  void jumpl();
+  void jumpe();
+  void jumpg();
+  void call();
+  void ret();
+  void read();
+  void write();
+  void halt(); 
+  void noop();
+};
+
+  
