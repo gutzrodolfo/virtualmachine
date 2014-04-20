2418de1108f931939b963be9ee7ad39164c026f1
Eli Gonzalez <004086484@coyote.csusb.edu>
2014-04-19 23:25:18 -0700

    OS combined only thing left is to add error checks

--- os.cpp
+++ os.cpp
@@ -6,9 +6,9 @@
 int main(int argc, char *argv[])
 {
 	//string name = argv[1];
-    Assembler as(argv[0]);
+    Assembler as(argv[1]);
     as.parse();
-    VirtualMachine vm(argv[0]);
+    VirtualMachine vm(argv[1]);
     vm.parse();
     return 0;
 } // main
